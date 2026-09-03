86228e6f651f2c7c6393e22fe7c4deb0b57c96d7
ethan_peng <78828540@qq.com>
2026-09-03 09:30:46 +0800

    ci: 添加标签自动发布流程

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.ethan.wdt"
-version = "0.1.0"
+version = providers.gradleProperty("pluginVersion").get()
 
 val localIdePath = providers.gradleProperty("intellijPlatform.localIdePath")
 
@@ -45,5 +45,15 @@ intellijPlatform {
         ideaVersion {
             sinceBuild = "262"
         }
+    }
+
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
     }
 }
